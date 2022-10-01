7bb640582dd9b7b22a674afe0fee2dea2bd0da76
Berryluxoo <lina.burkalo@gmail.com>
2022-10-01 23:50:31 +0100

    Change time

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
-  console.log(response.data);
   let currentTemp = document.querySelector("#temperature");
   currentTemp.innerHTML = Math.round(response.data.main.temp);
   let displayCity = document.querySelector("#city");
@@ -12,6 +34,8 @@ function showTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
